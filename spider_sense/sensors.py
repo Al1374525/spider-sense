0ab3b6bf739c98c75ccce91330598d0b6f35628c
Al1374525 <albertayiteyadjin@gmail.com>
2026-09-23 14:29:27 -0400

    Add SerialSensor implementing Sensor interface over a serial port

--- spider_sense/sensors.py
+++ spider_sense/sensors.py
@@ -1,4 +1,5 @@
 import random
+from spider_sense.protocol import PacketDecoder
 #Sensors class for spider_sense system
 class Sensor:
     def read(self):
@@ -21,3 +22,34 @@ class FakeSensor(Sensor):
             self.offset = self.schedule[self.sample_count]
         self.sample_count += 1
         return self.baseline + random.gauss(0, self.noise) + self.offset
+
+
+class SerialSensor(Sensor):
+    def __init__(self, port):
+        self.port = port
+        self.decoder = PacketDecoder()
+
+    def read(self):
+        #1. Check to see how many bytes are in waiting
+
+        waiting = self.port.in_waiting
+        #2. If non are waiting, return None
+        if waiting == 0:
+            return None
+        #3.Read exactly that many bytes 
+        data = self.port.read(waiting)
+
+        #4 feed them into the decoder
+        results = self.decoder.feed(data)
+
+        #5 if the results returned any temp, return the last one
+        if results:
+            return results[-1] #Grabs the last item in the list
+
+        return None
+
+        
+
+
+
+
